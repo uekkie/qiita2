8d05dd587d7446699e085d28ed09defc3ec4ccce
Hiroshi Ueki <hirocueki@gmail.com>
2018-05-06 22:32:10 +0900

    アイテムの投稿を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,34 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,only: [:new, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :new, :create, :edit, :update, :destroy]
 
-  # GET /items
-  # GET /items.json
   def index
-    @items = Item.all
   end
 
-  # GET /items/1
-  # GET /items/1.json
   def show
   end
 
-  # GET /items/new
   def new
     @item = Item.new
   end
 
-  # GET /items/1/edit
   def edit
   end
 
-  # POST /items
-  # POST /items.json
   def create
-    @item = Item.new(item_params)
+    @item = @user.items.new(item_params)
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.html { redirect_to [@user, @item], notice: '作成しました' }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
@@ -37,12 +30,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.html { redirect_to [@user, @item], notice: '更新しました' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -51,24 +42,28 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to user_url(@user), notice: '削除しました' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_user
+      if user_signed_in?
+        @user = current_user
+      else
+        @user = @item.user
+      end
+    end
+
     def item_params
-      params.fetch(:item, {})
+      params.require(:item).permit(:title, :body)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :title, :body, presence: true
 end
