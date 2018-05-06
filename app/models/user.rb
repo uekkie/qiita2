c4a1289ac1251599898b55005b7d9e5b12570cce
Hiroshi Ueki <hirocueki@gmail.com>
2018-05-06 18:39:47 +0900

    ユーザーコントローラーの追加（scaffold_controller）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :items
+  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
