8d6638621812c10ddfd0c062a8da36deb4fc7fe2
Hiroshi Ueki <hirocueki@gmail.com>
2018-05-13 17:41:52 +0900

    commentテーブルに制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513075953) do
+ActiveRecord::Schema.define(version: 20180513083759) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "body"
-    t.integer "user_id"
-    t.integer "item_id"
+    t.string "body", default: "", null: false
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_comments_on_item_id"
