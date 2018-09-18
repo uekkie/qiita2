44f0cf33904606bea73085fd5e86fa882cfc29de
Hiroshi Ueki <hirocueki@gmail.com>
2018-09-18 04:19:05 +0000

    コメントの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def index
-    # @users = User.all.order(:email)
     @users = User.page(params[:page])
   end
 
