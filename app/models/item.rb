8fe42a44d006b1426a80dbad0a1a9556ef42f79a
Hiroshi Ueki <hirocueki@gmail.com>
2018-09-21 09:25:23 +0000

    不要なコメント削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, :body, presence: true
-  
-  acts_as_ordered_taggable  # item.tag_list
+
+  acts_as_ordered_taggable
 end
