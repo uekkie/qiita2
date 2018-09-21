0814b5c3fbdc615a2ff185a6add59821fc96522c
Hiroshi Ueki <hirocueki@gmail.com>
2018-09-21 08:57:58 +0000

    ユーザー登録時の名前フィールドへの対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, only: [:update]
+  before_action :configure_permitted_parameters, only: [:create, :update]
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update) do |user|
-      user.permit(:name, :email, :password_confirmation, :current_password)
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :email, :password_confirmation, :current_password)
     end
   end
 end
