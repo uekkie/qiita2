8dd63e6437c017a5090780a055e3c36f193007ab
Hiroshi Ueki <hirocueki@gmail.com>
2018-09-27 10:34:29 +0000

    微修正

--- app/controllers/markdown_controller.rb
+++ app/controllers/markdown_controller.rb
@@ -1,6 +1,5 @@
 class MarkdownController < ApplicationController
   def preview
-    # @markdown_preview_content = params[:content]
     respond_to do |format|
       format.html { render html: view_context.markdown(params[:content]) }
     end
