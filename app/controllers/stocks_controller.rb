423b237f41e3f907bc00809856d3778332fcb060
Hiroshi Ueki <hirocueki@gmail.com>
2018-09-28 02:27:47 +0000

    ARを活用したオブジェクトの操作に

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,13 +2,13 @@ class StocksController < ApplicationController
   before_action :set_item
 
   def create
-    @stock = Stock.create(user_id: current_user.id, item_id: @item.id)
+    @stock = current_user.stocks.create!(user_id: current_user.id, item_id: @item.id)
     @stocks = Stock.where(item_id: @item.id)
     redirect_to user_item_path(@item.user, @item)
   end
 
   def destroy
-    stock = Stock.find_by(user_id: current_user.id, item_id: @item.id)
+    stock = current_user.stocks.find_by!(user_id: current_user.id, item_id: @item.id)
     stock.destroy
     @stocks = Stock.where(item_id: @item.id)
     redirect_to user_item_path(@item.user, @item)
