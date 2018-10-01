80b39db7e876a5b4d940c9b90a8b2a5b4a575008
Hiroshi Ueki <hirocueki@gmail.com>
2018-10-01 22:46:10 +0900

    FactoryBotからuserを生成するように

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,28 +3,21 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # メール、パスワードがあれば有効な状態であること
   it "is valid with a email and password" do
-    user = User.new(
-      email: "test@test.com",
-      password: "testuser"
-    )
+    user = create(:user)
     expect(user).to be_valid
   end
+
   # メールアドレスがなければ無効な状態であること
   it "is invalid without email address" do
-    user = User.new(email: nil)
+    user = build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("を入力してください")
   end
+
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
-    User.create(
-      email: "test@test.com",
-      password: "testuser"
-    )
-    user = User.new(
-      email: "test@test.com",
-      password: "testuser"
-    )
+    create(:user)
+    user = build(:user)
     user.valid?
     expect(user.errors[:email]).to include("はすでに存在します")
   end
