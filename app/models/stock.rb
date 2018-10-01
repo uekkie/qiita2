4ab8e0f4c0835a7437b8dfb20d45f1050076339a
Hiroshi Ueki <hirocueki@gmail.com>
2018-10-02 00:06:48 +0900

    stockにユニーク制約を追加

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,6 @@
 class Stock < ApplicationRecord
   belongs_to :user
   belongs_to :item, counter_cache: true
-  validates :user_id, :item_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :item_id }
+  validates :item_id, presence: true
 end
