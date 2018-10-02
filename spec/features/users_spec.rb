5916d2236c141081b4b008b98de692fcc9ab7c23
Hiroshi Ueki <hirocueki@gmail.com>
2018-10-02 09:05:53 +0000

    フィーチャスペックの追加

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,20 +1,46 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
+
   given(:user) { create(:user) }
 
-  # ユーザーはあたらしい投稿を作成することができる
-  scenario "user creates a new item" do
+  # ユーザーはログインができる
+  scenario "can login" do
     visit root_path
     click_link 'ログイン'
+
+    expect(current_path).to eq(new_user_session_path)
 
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: user.password
 
     click_button 'ログイン'
 
+    expect(page).to have_content 'ログインしました'
+  end
+
+  # ユーザーはログアウトができる
+  scenario "can logout" do
+    sign_in user
+    visit root_path
+    expect(page).to have_content "ログアウト"
+
+    click_link 'ログアウト'
+
+    expect(page).to have_content 'ログアウトしました'
+
+    expect(current_path).to eq(root_path)
+  end
+
+
+  # ユーザーはあたらしい投稿を作成することができる
+  scenario "creates a new item" do
+    sign_in user
+    visit new_user_item_path(user)
+
     click_link '投稿する'
     fill_in 'item_title', with: 'Test Item'
+    fill_in 'item_tag', with: 'Ruby,Rails'
     fill_in 'item_body', with: 'Trying out Capybara'
 
     expect {
@@ -23,5 +49,8 @@ RSpec.feature "Users", type: :feature do
 
     expect(page).to have_content '作成しました'
     expect(page).to have_content 'Test Item'
+    expect(page).to have_content 'Ruby'
+    expect(page).to have_content 'Rails'
+    expect(page).to have_content 'Trying out Capybara'
   end
 end
