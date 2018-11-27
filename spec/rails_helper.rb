6e0c4f4f9a40f9cea08a20b0c8c8944c28dd738c
Hiroshi Ueki <hirocueki@gmail.com>
2018-11-27 22:42:42 +0900

    Spec を修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -17,5 +16,9 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper, type: :feature
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'support/request_spec_helper'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -10,10 +8,4 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  config.include Devise::TestHelpers, type: :controller
-
-  config.include RequestSpecHelper, type: :request
-  config.include RequestSpecHelper, type: :feature
-
 end
