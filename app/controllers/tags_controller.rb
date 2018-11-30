7cfdcf31432d083c750b8a4d6cfc55660df50f96
Hiroshi Ueki <hirocueki@gmail.com>
2018-11-30 21:28:30 +0900

    不要なオプション any: true の削除

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,8 @@
 class TagsController < ApplicationController
   def show
     @tag_name = params[:name]
-    @items = Item.tagged_with(@tag_name, any: true)
-    @followers = User.tagged_with(@tag_name, any: true).count
+    @items = Item.tagged_with(@tag_name)
+    @followers = User.tagged_with(@tag_name).count
   end
 
   def create
