93a094047c48a73d8026e14aedf377f3b8ce429d
Hiroshi Ueki <hirocueki@gmail.com>
2018-12-28 14:43:07 +0900

    よりRailsらしい記述で簡潔に

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,8 +2,7 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    follower_ids = Relationship.where(follower_id: current_user.id).pluck(:followed_id)
-    @items = Item.where(user_id: follower_ids).recent.page
+    @items = Item.where(user: current_user.following).recent.page
     @user = current_user
   end
 end
