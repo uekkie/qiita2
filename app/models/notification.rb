ea340598c396753b5913d68bdc59c6ace54b15bb
Hiroshi Ueki <hirocueki@gmail.com>
2019-01-21 15:00:26 +0900

    未読通知の scope を追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,6 @@ class Notification < ApplicationRecord
   belongs_to :owner, class_name: "User"
   belongs_to :sender, class_name: "User"
   validates :owner_id, :sender_id, presence: true
+
+  scope :unread, -> { includes(:owner, :sender, :item).where(confirmed: false).order(created_at: :desc).limit(5) }
 end
